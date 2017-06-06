ff69d77ade46ab7f0d226c9742d9d0f485822510
RaodahSandouk <iris.ite@hotmail>
2017-06-07 01:36:02 +0300

    Add ObjectDrawing && Material Classes

---
+++ Vr_Gl/DrawingObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Vr_Gl
+{
+    abstract class DrawingObject
+    {
+        protected int Texture;
+        protected Material Material;
+        protected Vector3[] Vertdata = new Vector3[] { new Vector3(-1f, -1f,  -1f),
+                new Vector3(-1f, -1f,  1f),
+                new Vector3(1f, -1f,  1f),
+                new Vector3(1f, 1f,  1f),
+                new Vector3(-1f, 1f,  1f),
+
+                
+                new Vector3(-1f, -1f,  -1f),
+                new Vector3(-1f, 1f,  -1f),
+
+                new Vector3(1f, -1f,  -1f),
+                new Vector3(1f, 1f,  -1f),
+                new Vector3(-1f, 1f,  -1f),
+                new Vector3(-1f, -1f,  1f),
+                new Vector3(1f, -1f,  1f),
+                new Vector3(1f, 1f,  1f),
+                new Vector3(-1f, 1f,  1f)
+            };
+
+        public abstract void Draw()
+
+            //GL.PushMatrix();
+            //GL.Enable(EnableCap.Texture2D);
+            //// _material.Activate();
+            //GL.LoadIdentity();
+
+            ////GL.Color3(Color.Aqua);
+
+            //GL.Translate(0.0f, 0.0f, -6.0f);
+            //GL.Rotate(180, 0, 0, 1);
+            //GL.Scale(1, .8f, 1);
+            //GL.BindTexture(TextureTarget.ProxyTexture2D, Texture);
+            //GL.Begin(mode: BeginMode.Quads);
+            //// Front Face
+            //GL.Normal3(0.0f, 0.0f, 1.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            //// Back Face
+            //GL.Normal3(0.0f, 0.0f, -1.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            //// Top Face
+            //GL.Normal3(0.0f, 1.0f, 0.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            //// Bottom Face
+            //GL.Normal3(0.0f, -1.0f, 0.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            //// Right face
+            //GL.Normal3(1.0f, 0.0f, 0.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            //// Left Face
+            //GL.Normal3(-1.0f, 0.0f, 0.0f);
+            //GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            //GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            //GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            //GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            //GL.End();
+            //GL.Disable(EnableCap.Texture2D);
+            //GL.PopMatrix();
+            ;
+    }
+}
---
+++ Vr_Gl/Graphics/Material.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+
+namespace VR_Project
+{
+    class Material
+    {
+        float ambient_r;
+        float ambient_g;
+        float ambient_b;
+
+        float diffuse_r;
+        float diffuse_g;
+        float diffuse_b;
+
+        float specular_r;
+        float specular_g;
+        float specular_b;
+
+        float shiness = 128.0f;
+
+        public Material(float ar, float ag, float ab, float dr, float dg, float db, float sr, float sg, float sb, float s)
+        {
+            ambient_r = ar; ambient_g = ag; ambient_b = ab;
+            diffuse_r = dr; diffuse_g = dg; diffuse_b = db;
+            specular_r = sr; specular_g = sg; specular_b = sb;
+            shiness *= s;
+        }
+
+        public void Activate()
+        {
+            GL.Enable(EnableCap.ColorMaterial);
+		    float []ambient = { ambient_r, ambient_g, ambient_b, 1.0f };
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Ambient, ambient);
+		    float []diffuse = { diffuse_r, diffuse_g, diffuse_b };
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Diffuse, diffuse);
+		    float []specular = { specular_r, specular_g, specular_b };
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Specular, specular);
+		    float []shines = { shiness };
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Shininess, shines);
+
+	    }
+    }
+}
